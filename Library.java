c768d80ae7cc413fbca98aa1b68a205b5eba801f
Ozziekins <nenubariozioma@gmail.com>
2021-04-19 17:25:30 +0300

    added library

--- Library.java
+++ Library.java
@@ -1,3 +1,14 @@
+import java.util.Arrays;
+
 public class Library {
-    
+    String[] books;
+
+    public String[] getBooks() {
+        return this.books;
+    }
+
+    public void addBook(String book) {
+        this.books = Arrays.copyOf(this.books, this.books.length + 1);
+        this.books[this.books.length - 1] = book;
+    }
 }
